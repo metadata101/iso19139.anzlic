2cf779b588c7e3bfbf85813ec737fc2a87c7d8b3
Simon Pigot <Simon.Pigot@csiro.au>
2015-11-19 19:11:14 +1100

    Add in iso19139.anzlic from 2.10.x

---
+++ src/main/java/org/fao/geonet/schema/iso19139anzlic/ISO19139ANZLICNamespaces.java
@@ -0,0 +1,17 @@
+package org.fao.geonet.schema.iso19139anzlic;
+
+import org.jdom.Namespace;
+
+/**
+ * Namespaces for iso19139.anzlic metadata standard.
+ */
+public class ISO19139ANZLICNamespaces {
+    public static final Namespace GCO =
+            Namespace.getNamespace("gco", "http://www.isotc211.org/2005/gco");
+    public static final Namespace SRV =
+            Namespace.getNamespace("srv", "http://www.isotc211.org/2005/srv");
+    public static final Namespace GMD =
+            Namespace.getNamespace("gmd", "http://www.isotc211.org/2005/gmd");
+    public static final Namespace GMX =
+            Namespace.getNamespace("gmx", "http://www.isotc211.org/2005/gmx");
+}
---
+++ src/main/java/org/fao/geonet/schema/iso19139anzlic/ISO19139ANZLICSchemaPlugin.java
@@ -0,0 +1,211 @@
+package org.fao.geonet.schema.iso19139anzlic;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.fao.geonet.kernel.schema.AssociatedResource;
+import org.fao.geonet.kernel.schema.AssociatedResourcesSchemaPlugin;
+import org.fao.geonet.kernel.schema.ISOPlugin;
+import org.fao.geonet.kernel.schema.MultilingualSchemaPlugin;
+import org.fao.geonet.utils.Log;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.filter.ElementFilter;
+import org.jdom.xpath.XPath;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by francois on 6/15/14. Minor modification for anzlic by sppigot.
+ */
+public class ISO19139ANZLICSchemaPlugin
+        extends org.fao.geonet.kernel.schema.SchemaPlugin
+        implements
+                AssociatedResourcesSchemaPlugin,
+                MultilingualSchemaPlugin,
+                ISOPlugin {
+    public static final String IDENTIFIER = "iso19139.mcp";
+
+    private static ImmutableSet<Namespace> allNamespaces;
+    private static Map<String, Namespace> allTypenames;
+    static {
+        allNamespaces = ImmutableSet.<Namespace>builder()
+                .add(ISO19139ANZLICNamespaces.GCO)
+                .add(ISO19139ANZLICNamespaces.GMD)
+                .add(ISO19139ANZLICNamespaces.SRV)
+                .add(ISO19139ANZLICNamespaces.GMX)
+                .build();
+        allTypenames = ImmutableMap.<String, Namespace>builder()
+                .put("csw:Record", Namespace.getNamespace("csw", "http://www.opengis.net/cat/csw/2.0.2"))
+                .put("gmd:MD_Metadata", ISO19139ANZLICNamespaces.GMD)
+                .build();
+    }
+
+    public ISO19139ANZLICSchemaPlugin() {
+        super(IDENTIFIER);
+    }
+
+    /**
+     * Return sibling relation defined in aggregationInfo.
+     *
+     * @param metadata
+     * @return
+     */
+    public Set<AssociatedResource> getAssociatedResourcesUUIDs(Element metadata) {
+
+        String XPATH_FOR_AGGRGATIONINFO = "*//gmd:aggregationInfo/*" +
+                "[gmd:aggregateDataSetIdentifier/*/gmd:code " +
+                "and gmd:associationType/gmd:DS_AssociationTypeCode/@codeListValue!='']";
+        Set<AssociatedResource> listOfResources = new HashSet<AssociatedResource>();
+        List<?> sibs = null;
+        try {
+            sibs = Xml.selectNodes(
+                            metadata,
+                            XPATH_FOR_AGGRGATIONINFO,
+                            allNamespaces.asList());
+
+
+            for (Object o : sibs) {
+                try {
+                    if (o instanceof Element) {
+                        Element sib = (Element) o;
+                        Element agId = (Element) sib.getChild("aggregateDataSetIdentifier", ISO19139ANZLICNamespaces.GMD)
+                                .getChildren().get(0);
+                        String sibUuid = getChild(agId, "code", ISO19139ANZLICNamespaces.GMD)
+                                .getChildText("CharacterString", ISO19139ANZLICNamespaces.GCO);
+                        final Element associationTypeEl = getChild(sib, "associationType", ISO19139ANZLICNamespaces.GMD);
+                        String associationType = getChild(associationTypeEl, "DS_AssociationTypeCode", ISO19139ANZLICNamespaces.GMD)
+                                .getAttributeValue("codeListValue");
+                        final Element initiativeTypeEl = getChild(sib, "initiativeType", ISO19139ANZLICNamespaces.GMD);
+                        String initiativeType = "";
+                        if (initiativeTypeEl != null) {
+                            initiativeType = getChild(initiativeTypeEl, "DS_InitiativeTypeCode", ISO19139ANZLICNamespaces.GMD)
+                                    .getAttributeValue("codeListValue");
+                        }
+                        AssociatedResource resource = new AssociatedResource(sibUuid, initiativeType, associationType);
+                        listOfResources.add(resource);
+                    }
+                } catch (Exception e) {
+                    Log.error(Log.JEEVES, "Error getting resources UUIDs", e);
+                }
+            }
+        } catch (Exception e) {
+            Log.error(Log.JEEVES, "Error getting resources UUIDs", e);
+        }
+        return listOfResources;
+    }
+
+    private Element getChild(Element el, String name, Namespace namespace) {
+        final Element child = el.getChild(name, namespace);
+        if (child == null) {
+            return new Element(name, namespace);
+        }
+        return child;
+    }
+
+    @Override
+    public Set<String> getAssociatedParentUUIDs(Element metadata) {
+        ElementFilter elementFilter = new ElementFilter("parentIdentifier", ISO19139ANZLICNamespaces.GMD);
+        return Xml.filterElementValues(
+                metadata,
+                elementFilter,
+                "CharacterString", ISO19139ANZLICNamespaces.GCO,
+                null);
+    }
+
+    public Set<String> getAssociatedDatasetUUIDs(Element metadata) {
+        return getAttributeUuidrefValues(metadata, "operatesOn", ISO19139ANZLICNamespaces.SRV);
+    }
+
+    public Set<String> getAssociatedFeatureCatalogueUUIDs(Element metadata) {
+        return getAttributeUuidrefValues(metadata, "featureCatalogueCitation", ISO19139ANZLICNamespaces.GMD);
+    }
+
+    public Set<String> getAssociatedSourceUUIDs(Element metadata) {
+        return getAttributeUuidrefValues(metadata, "source", ISO19139ANZLICNamespaces.GMD);
+    }
+
+    private Set<String> getAttributeUuidrefValues(Element metadata, String tagName, Namespace namespace) {
+        ElementFilter elementFilter = new ElementFilter(tagName, namespace);
+        return Xml.filterElementValues(
+                metadata,
+                elementFilter,
+                null, null,
+                "uuidref");
+    }
+
+    @Override
+    public List<Element> getTranslationForElement(Element element, String languageIdentifier) {
+        final String path = ".//gmd:LocalisedCharacterString" +
+                "[@locale='#" + languageIdentifier + "']";
+        try {
+            XPath xpath = XPath.newInstance(path);
+            @SuppressWarnings("unchecked")
+            List<Element> matches = xpath.selectNodes(element);
+            return matches;
+        } catch (Exception e) {
+            Log.debug(LOGGER_NAME, getIdentifier() + ": getTranslationForElement failed " +
+                    "on element " + Xml.getString(element) +
+                    " using XPath '" + path +
+                    "updatedLocalizedTextElement exception " + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     *  Add a LocalisedCharacterString to an element. In ISO19139, the translation are
+     *  stored gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString.
+     *
+     * <pre>
+     * <gmd:title xsi:type="gmd:PT_FreeText_PropertyType">
+     *    <gco:CharacterString>Template for Vector data in ISO19139 (multilingual)</gco:CharacterString>
+     *    <gmd:PT_FreeText>
+     *        <gmd:textGroup>
+     *            <gmd:LocalisedCharacterString locale="#FRE">Modèle de données vectorielles en ISO19139 (multilingue)</gmd:LocalisedCharacterString>
+     *        </gmd:textGroup>
+     * </pre>
+     *
+     * @param element
+     * @param languageIdentifier
+     * @param value
+     */
+    @Override
+    public void addTranslationToElement(Element element, String languageIdentifier, String value) {
+        // An ISO19139 element containing translation has an xsi:type attribute
+        element.setAttribute("type", "gmd:PT_FreeText_PropertyType",
+                Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance"));
+
+        // Create a new translation for the language
+        Element langElem = new Element("LocalisedCharacterString", ISO19139ANZLICNamespaces.GMD);
+        langElem.setAttribute("locale", "#" + languageIdentifier);
+        langElem.setText(value);
+        Element textGroupElement = new Element("textGroup", ISO19139ANZLICNamespaces.GMD);
+        textGroupElement.addContent(langElem);
+
+        // Get the PT_FreeText node where to insert the translation into
+        Element freeTextElement = element.getChild("PT_FreeText", ISO19139ANZLICNamespaces.GMD);
+        if (freeTextElement == null) {
+            freeTextElement = new Element("PT_FreeText", ISO19139ANZLICNamespaces.GMD);
+            element.addContent(freeTextElement);
+        }
+        freeTextElement.addContent(textGroupElement);
+    }
+
+    @Override
+    public String getBasicTypeCharacterStringName() {
+        return "gco:CharacterString";
+    }
+
+    @Override
+    public Element createBasicTypeCharacterString() {
+        return new Element("CharacterString", ISO19139ANZLICNamespaces.GCO);
+    }
+
+    @Override
+    public Map<String, Namespace> getCswTypeNames() {
+        return allTypenames;
+    }
+}
